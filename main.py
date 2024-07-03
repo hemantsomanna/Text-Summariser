19fa3db00c53feee76840dc14eb2b65c2edb0287
hemantsomanna <90712144+hemantsomanna@users.noreply.github.com>
2024-07-03 16:05:22 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
+from textsummariser.pipeline.stage1data_ingestion import DataIngestionTrainingPipeline
 from textsummariser.logging import logger
 
-logger.info("WELCOME")
-
-
-
-
-
-
-
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info({STAGE_NAME})
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info("COMPLETED")
+except Exception as e:
+        logger.exception(e)
+        raise e 
