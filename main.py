9f039d131087adde517b6bd1ceaaebaac114d0bc
hemantsomanna <90712144+hemantsomanna@users.noreply.github.com>
2024-07-03 23:24:33 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from textsummariser.pipeline.stage1data_ingestion import DataIngestionTrainingPipeline
 from textsummariser.logging import logger
+from textsummariser.pipeline.stage2data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = "Data Validation Stage"
 try:
     logger.info({STAGE_NAME})
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
+    data_validation = DataValidationTrainingPipeline()
+    data_validation .main()
     logger.info("COMPLETED")
 except Exception as e:
         logger.exception(e)
