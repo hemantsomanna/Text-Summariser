d0e17c67d06dc879c74cf943bb5f2b08f68a00d9
hemantsomanna <90712144+hemantsomanna@users.noreply.github.com>
2024-07-05 23:57:09 +0530

    data transformation ->  model training -> model evaluation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from textsummariser.logging import logger
 from textsummariser.pipeline.stage2data_validation import DataValidationTrainingPipeline
 from textsummariser.pipeline.stage3data_transformation import DataTransformationTrainingPipeline
 from textsummariser.pipeline.stage4model_trainer import ModelTrainerTrainingPipeline
+from textsummariser.pipeline.stage5model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Model Trainer stage"
 try: 
@@ -10,6 +11,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
